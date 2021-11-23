ff884b4fddd950ec8349bdc4fc1ed4dd5cc9abbe
emmanuel <gbengacode>
2021-11-23 11:14:38 +0100

    Add addBook method in the bookClass

--- js/bookClass.js
+++ js/bookClass.js
@@ -11,13 +11,33 @@ class Book {
     this.ul.style.listStyleType = 'none';
     this.content.appendChild(this.ul);
     this.container.insertAdjacentElement('beforebegin', this.content);
-    this.submitBtn = document.querySelector('.submit');
+    this.submitBtn = document.querySelector('.btn-add');
     this.title = document.querySelector('.title');
     this.author = document.querySelector('.author');
     this.bookData = [];
+  }
+
+  addBook() {
+    this.submitBtn.addEventListener('click', (e) => {
+      e.preventDefault();
+      const titleVal = this.title.value;
+      const authorVal = this.author.value;
+      if (titleVal.trim() === '') return;
+      if (authorVal.trim() === '') return;
+      this.bookData.push({ titleVal, authorVal });
+      localStorage.setItem('book', JSON.stringify(this.bookData));
+      this.ul.innerHTML += `<li>
+                   <p>${titleVal}</p>
+                   <p>${authorVal}</p>
+                   <button class="remove" value=${titleVal} >Remove</button>
+                   <hr/>
+                   </li>`;
+      this.form.reset();
+    });
   }
 }
 
 const title = document.querySelector('.title');
 const author = document.querySelector('.author');
-Book(title, author);
+const book = new Book(title, author);
+book.addBook();
