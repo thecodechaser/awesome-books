46bf237dfd91b6945aefb6e41cf1579f60cdb1ec
emmanuel <gbengacode>
2021-11-23 10:55:50 +0100

    Add other properties of the book class

--- js/bookClass.js
+++ js/bookClass.js
@@ -1,10 +1,23 @@
 class Book {
-  constructor(title, author){
-    this.title=title;
-    this.author=author;
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.form = document.querySelector('form');
+    this.div = document.createElement('div');
+    this.container = document.querySelector('.container');
+    this.content = document.createElement('div');
+    this.ul = document.createElement('ul');
+    this.content.classList.add('list-of-book');
+    this.ul.style.listStyleType = 'none';
+    this.content.appendChild(this.ul);
+    this.container.insertAdjacentElement('beforebegin', this.content);
+    this.submitBtn = document.querySelector('.submit');
+    this.title = document.querySelector('.title');
+    this.author = document.querySelector('.author');
+    this.bookData = [];
   }
 }
 
-const title = document.querySelector(".title");
-const author = document.querySelector(".author");
-const book= new Book(title, author);
+const title = document.querySelector('.title');
+const author = document.querySelector('.author');
+Book(title, author);
