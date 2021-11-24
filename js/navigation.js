cb5b008e6572d4d4afc9b13364c6e99ba0dac50f
emmanuel <gbengacode>
2021-11-24 15:24:46 +0100

    Add navigation based on click

---
+++ js/navigation.js
@@ -0,0 +1,61 @@
+export const navigatorHelper = () => {
+    const menuList = document.querySelector('.desktop-menu');
+    const bookList = document.querySelector('.book-list-c');
+    const hr = document.querySelector('hr');
+    const formContainer = document.querySelector('.form-container');
+    const contact = document.querySelector('.contact-section');
+    const container = document.querySelector('.mobile-menu-c');
+   
+   
+     menuList.addEventListener('click', (e) => {
+         if(e.target.classList.contains('list')){
+            formContainer.style.display = 'none';
+            contact.style.display = 'none';
+            bookList.style.display = 'block'
+           
+         }
+        
+         if(e.target.classList.contains('new')){
+            
+             bookList.style.display = 'none';
+             hr.style.display = 'none';
+             contact.style.display = 'none'
+             formContainer.style.display = 'flex';
+            
+            
+         }
+
+         if(e.target.classList.contains('contact')){
+            contact.style.display = 'flex'
+            bookList.style.display = 'none';
+            hr.style.display = 'none';
+            formContainer.style.display = 'none';
+         
+         }
+     })
+
+     const menuListMov = document.querySelector('.mobile-nav');  
+     menuListMov.addEventListener('click',  (e) => {
+         if(e.target.classList.contains('list')){
+            formContainer.style.display = 'none';
+            contact.style.display = 'none';
+            bookList.style.display = 'block'
+            container.style.display = 'none'
+         }
+         if(e.target.classList.contains('new')){
+            bookList.style.display = 'none';
+            hr.style.display = 'none';
+            contact.style.display = 'none'
+            formContainer.style.display = 'flex';
+            container.style.display = 'none'
+         }
+         if(e.target.classList.contains('contact')){
+            contact.style.display = 'flex'
+            bookList.style.display = 'none';
+            hr.style.display = 'none';
+            formContainer.style.display = 'none';
+            container.style.display = 'none'
+         }
+     })   
+}
+
